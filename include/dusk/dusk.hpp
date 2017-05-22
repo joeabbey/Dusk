656f21c40ae8d8f8e8a0467229960895ee013917
Stephen Lane-Walsh <sdl.slane@gmail.com>
2017-05-22 00:37:45 -0400

    Initial commit

---
+++ include/dusk/dusk.hpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+#include <dusk/config.hpp>
+
+namespace dusk {
+
+void PrintVersion();
+
+} // namespace dusk
---
+++ src/dusk.cpp
@@ -0,0 +1,10 @@
+#include "dusk/dusk.hpp"
+
+namespace dusk {
+
+void PrintVersion()
+{
+    printf("Dusk Version: %s\n", DUSK_VERSION);
+}
+
+} // namespace dusk
